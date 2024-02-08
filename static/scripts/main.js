2074ed922e50c8b6a9607096fd37208b87d914f9
dmicas <65303708+dmicas@users.noreply.github.com>
2024-02-08 22:00:25 +0200

    created auto scroll to contact-us section

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,10 +1,28 @@
 window.addEventListener("scroll", () => {
   const navbar = document.getElementById("navbar");
-  console.log(navbar)
+  console.log(navbar);
   if (window.scrollY > 20) {
     // Adjust threshold as needed
     navbar.classList.add("nav-scrolled");
   } else {
     navbar.classList.remove("nav-scrolled");
   }
+});
+
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const targetSection = document.getElementById(link.hash.slice(1));
+    const offset = targetSection.offsetTop - 44;
+
+    if (targetSection) {
+      window.scrollTo({
+        top: offset,
+        behavior: "smooth",
+      });
+    }
+  });
 });
